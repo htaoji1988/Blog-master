3e326f67a533c9a0a2e5cf7fc197c68a373f01bc
haitao.ji <htaoji1988@126.com>
2018-11-16 15:17:33 +0800

    Initial commit

---
+++ banner/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+class BannerAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title', 'article', 'img', 'img_xs', 'create_time')
+    list_filter = ('create_time',)
+    fields = ('title', 'article', 'img', 'img_xs', 'summary')
+
+
+admin.site.register(Banner, BannerAdmin)
